38647b6d1901679ea5af85f4b11f044ef97f1d4c
Patrick Miller <apickledwalrus@icloud.com>
2026-05-21 14:39:35 -0400

    Always use API fetched versions for syntax page generation

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@ declare namespace App {
     interface Locals {
         syntaxesData: {
             addons: string[],
+            version: string,
             types: string[],
         },
     }
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
@@ -25,5 +25,23 @@ export const collections = {
             pattern: '*.json',
             base: './src/assets/syntaxes/addons/',
         }),
+    }),
+    skriptReleasesJson: defineCollection({
+       loader: async() =>
+           fetch('https://api.github.com/repos/SkriptLang/Skript/releases?per_page=100', {
+               headers: {
+                   'Accept': 'application/vnd.github+json',
+                   'X-GitHub-Api-Version': '2022-11-28',
+                   'User-Agent': 'SkriptLang',
+               },
+           }).then(response => {
+               if (!response.ok) {
+                   throw new Error("Failed to fetch versions... aborting");
+               }
+               return response.json();
+           }).then(json => json.map((release: any) => ({
+               ...release,
+               id: release.id.toString(),
+           })))
     }),
 };
