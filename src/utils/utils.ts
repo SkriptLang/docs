a710955dc6bca93281f96423ff100550852a89d5
Patrick Miller <apickledwalrus@icloud.com>
2026-07-29 11:10:22 -0400

    Generalize version utilities

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -46,6 +46,10 @@ export function clearParams() {
     window.history.replaceState({}, '', window.location.pathname);
 }
 
+// ---------------------
+// Word Utilities
+// ---------------------
+
 export function getPlural(string: string) : string {
     if (string.endsWith("y")) {
         return string.slice(0, -1) + "ies";
@@ -55,4 +59,87 @@ export function getPlural(string: string) : string {
 
 export function changeSingular(string: string, plural: boolean) {
     return plural ? getPlural(string) : string;
+}
+
+// ---------------------
+// Version Utilities
+// ---------------------
+
+export interface Version {
+    major: number;
+    minor: number;
+    patch: number;
+    tag?: string;
+}
+
+export function getVersionAsString(version: Version): string {
+    if (version.major === Infinity) {
+        return version.tag === undefined ? '' : version.tag;
+    }
+    return version.major + '.' + version.minor + '.' + version.patch + (version.tag === undefined ? '' : version.tag);
+}
+
+const versionPattern = /(?:(\d+)\.(\d+)(?:\.(\d+))?)?(.*)/i;
+export function parseVersion(version: string): Version {
+    const matches = versionPattern.exec(version);
+    if (!matches) {
+        throw new Error();
+    }
+    return extractVersionFromMatches(matches);
+}
+
+const versionInStringPattern = /.*?(\d+)\.(\d+)(?:\.(\d+))?(.*)/i;
+export function parseVersionInString(version: string): Version | null {
+    const matches = versionInStringPattern.exec(version);
+    if (!matches) {
+        return null;
+    }
+    return extractVersionFromMatches(matches);
+}
+
+function extractVersionFromMatches(matches: RegExpExecArray): Version {
+    return {
+        major: matches[1] === undefined ? Infinity : parseInt(matches[1]),
+        minor: matches[2] === undefined ? Infinity : parseInt(matches[2]),
+        patch: matches[3] === undefined ? 0 : parseInt(matches[3]),
+        tag: matches[4],
+    }
+}
+
+/**
+ * @return Whether <code>a</code> is less than, equal to, or greater than <code>b</code>.
+ * Expressed as -1, 0, or 1 respectively.
+ */
+export function compareVersions(a: Version, b: Version, descending?: boolean) : number {
+    if (a.major !== Infinity) {
+        if (b.major == Infinity) { // "b" is not really a version, so "a" is first
+            return -1;
+        }
+        if (a.major > b.major) {
+            return descending ? -1 : 1;
+        } else if (a.major < b.major) {
+            return descending ? 1 : -1;
+        } else if (a.minor > b.minor) {
+            return descending ? -1 : 1;
+        } else if (a.minor < b.minor) {
+            return descending ? 1 : -1;
+        } else if (a.patch > b.patch) {
+            return descending ? -1 : 1;
+        } else if (a.patch < b.patch) {
+            return descending ? 1 : -1;
+        }
+    } else if (b.major !== Infinity) { // "a" is not really a version, so "b" is first
+        return 1;
+    }
+
+    if (a.tag) {
+        if (b.tag) { // both have tags
+            return a.tag.localeCompare(b.tag) * (descending ? 1 : -1);
+        }
+        return 1; // "a" has a tag but "b" does not
+    } else if (b.tag) { // "b" has a tag but "a" does not
+        return -1;
+    }
+
+    return 0;
 }
